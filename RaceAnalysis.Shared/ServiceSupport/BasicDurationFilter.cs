562f8b3546d25b80b7ec6c2d8e822f3a8377c10b
Scot <Scot@SCOT-PC>
2016-11-08 15:22:46 -0600

    Fix Title issue

--- RaceAnalysis.Shared/ServiceSupport/BasicDurationFilter.cs
+++ RaceAnalysis.Shared/ServiceSupport/BasicDurationFilter.cs
@@ -1,10 +1,6 @@
-﻿using RaceAnalysis.Models;
-using RaceAnalysis.Service.Interfaces;
+﻿using RaceAnalysis.Service.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace RaceAnalysis.ServiceSupport
 {
