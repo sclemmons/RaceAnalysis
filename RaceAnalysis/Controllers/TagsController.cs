5a44caff69c23a3fd0b3757cf92e3b708b5d2549
Scot <Scot@SCOT-PC>
2017-01-02 15:58:15 -0600

    First version of edit all Tags

--- RaceAnalysis/Controllers/TagsController.cs
+++ RaceAnalysis/Controllers/TagsController.cs
@@ -106,6 +106,93 @@ namespace RaceAnalysis.Controllers
             return View(tag);
         }
 
+        public ActionResult EditAll()
+        {
+            var viewModel = new TagViewModel();
+            viewModel.Tags = db.Tags.ToList();
+
+            return View("EditAll",viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public ActionResult Edit(
+        // [Bind(Prefix="Race.Conditions",Include = "RaceConditionsId,SwimLayout,BikeLayout,RunLayout")] RaceConditions raceConditions)
+        public ActionResult EditAll(SimpleRaceConditionsViewModel postedModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Save(postedModel);
+                return RedirectToAction("Index");
+            }
+
+            var tagviewModel = new TagViewModel();
+            tagviewModel.Tags = db.Tags.ToList();
+
+            return View("EditAll", tagviewModel);
+        }
+
+        private void Save(SimpleRaceConditionsViewModel viewModel)
+        {
+            Save(viewModel.selectedSwimLayout, TagType.SwimLayout);
+            Save(viewModel.selectedSwimMedium, TagType.SwimMedium);
+            Save(viewModel.selectedSwimOther, TagType.SwimOther);
+            Save(viewModel.selectedSwimWeather, TagType.SwimWeather);
+
+
+            Save(viewModel.selectedBikeLayout, TagType.BikeLayout);
+            Save(viewModel.selectedBikeMedium, TagType.BikeMedium);
+            Save(viewModel.selectedBikeOther, TagType.BikeOther);
+            Save(viewModel.selectedBikeWeather, TagType.BikeWeather);
+
+
+            Save(viewModel.selectedRunLayout, TagType.RunLayout);
+            Save(viewModel.selectedRunMedium, TagType.RunMedium);
+            Save(viewModel.selectedRunOther, TagType.RunOther);
+            Save(viewModel.selectedRunWeather, TagType.RunWeather);
+
+        }
+        private void Save(List<String> selectedValues, TagType tagType)
+        {
+            if (selectedValues == null)
+            {
+                    return; //we have no further business to do
+            }
+            foreach (string s in selectedValues)
+            {
+
+                Tag tag;
+                int tagId;
+                bool result = Int32.TryParse(s, out tagId);
+                //tag is either a tag id or a new tag value that the user typed
+                if (result)
+                {
+                    tag = db.Tags.Find(tagId);
+
+                }
+                else //a new tag
+                {
+                    tag = new Tag
+                    {
+                        Value = s,
+                        Type = tagType
+
+                    };
+
+                    db.Tags.Add(tag);
+                }
+            }
+
+            db.SaveChanges();
+
+
+        }
+
+
+
+
+
         // POST: Tags/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
