d0c7fc064695cae7f207c8022e43c7a268339b45
Scot <Scot@SCOT-PC>
2017-05-19 21:08:50 -0500

    f

--- CleanRequestContextsAndDeleteAllTriathletes.sql
+++ CleanRequestContextsAndDeleteAllTriathletes.sql
@@ -1,10 +1,10 @@
 
-select * from requestcontexts where raceid='himral2016' order by AgeGroupId
+select * from requestcontexts where raceid='implacid2014' order by AgeGroupId
 
 
 delete Triathletes 
 where RequestContextId in
 (select RequestContextId from RequestContexts 
-where raceid='himral2015')
+where raceid='implacid2014')
 
-delete RequestContexts where raceId = 'himral2015' 
+delete RequestContexts where raceId = 'implacid2014' 
