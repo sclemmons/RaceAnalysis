3a1b169febf5c6b3de8a68ddb7bb8d5fcf9aef81
Scot <Scot@SCOT-PC>
2017-06-21 20:28:42 -0500

    some attempts to improve the typeahead with cache. still not working that well in production. looking for different solutions

---
+++ RaceAnalysis/App_Start/CacheConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaceAnalysis
+{
+    public class CacheConfig
+    {
+        public static void Register()
+        {
+
+        }
+
+
+    }
+}
