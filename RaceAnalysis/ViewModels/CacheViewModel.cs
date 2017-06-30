20be2f19bb4d3cf7c2017644c3ae615774a6857c
Scot <Scot@SCOT-PC>
2017-06-29 20:46:39 -0500

    Checking in this version of Athlete Search - reworked with redis cache. it is working but not fast enough.  went back to database which is a little faster. going to try to implement fulltext search with db

---
+++ RaceAnalysis/ViewModels/CacheViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaceAnalysis.Models
+{
+    public class CacheViewModel
+    {
+        public int ShallowAthleteCount { get; set; }
+    }
+}
