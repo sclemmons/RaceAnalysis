9e531dd9ae6aa12efcc07c9fd5575761ec23b5ca
Scot <Scot@SCOT-PC>
2017-09-10 19:20:23 -0500

    checking in new files

---
+++ RaceAnalysis/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using RaceAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RaceAnalysis.Models 
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<Race> RecentRaces { get; set; }
+        public IEnumerable<RaceAggregate> FastestRuns { get; set; }
+        public IEnumerable<RaceAggregate> FastestBikes { get; set; }
+        public IEnumerable<RaceAggregate> FastestSwims { get; set; }
+        public IEnumerable<RaceAggregate> FastestFinishes { get; set; }
+
+        public IEnumerable<Triathlete> FastestAthleteFinishes { get; set; }
+
+
+    }
+}
